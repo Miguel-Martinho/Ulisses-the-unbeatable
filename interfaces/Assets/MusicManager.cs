3d2ceca48298400c3afa4f6138b4286aec4925bb
VoidVariable <thevoidvariable@gmail.com>
2022-01-04 19:39:43 +0000

    Add music and end level screen

---
+++ interfaces/Assets/MusicManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class MusicManager : MonoBehaviour
+{
+    AudioSource source;
+
+    [SerializeField]
+    AudioClip clip;
+    private static MusicManager _instance;
+
+    public static MusicManager Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+        source = GetComponent<AudioSource>();
+        source.clip = clip;
+        source.Play();
+    }
+
+
+}
